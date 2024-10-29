b9c20006903728a6c301b087960e4a62b17de111
XiaoMing <2910796610@qq.com>
2024-10-29 16:43:54 +0800

    part3

--- mainwindow.h
+++ mainwindow.h
@@ -18,11 +18,43 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionAbout_triggered();
+
+    void on_actionFind_triggered();
+
+    void on_actionReplace_triggered();
+
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSaveAs_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_textEdit_textChanged();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionCopy_triggered();
+
 private:
     Ui::MainWindow *ui;
 
     //状态栏内容定义
     QLabel statusCursorLabel;
     QLabel statusLabel;
+
+    QString filePath;
+    bool textChanged;
+
+    bool userEditConfirmed();
 };
 #endif // MAINWINDOW_H
